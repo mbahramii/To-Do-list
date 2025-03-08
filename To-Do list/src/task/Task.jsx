96d59960a77fc6f0ab87fdd773f49c2df6a28385
mbahramii <bahramiimohammad92@gmail.com>
2025-03-08 06:06:45 +0330

    add icons for check and task delete , and add style for completed tasks

--- To-Do list/src/task/Task.jsx
+++ To-Do list/src/task/Task.jsx
@@ -1,14 +1,23 @@
-import './Task.css'
+import './Task.css';
+import { FaTrash } from "react-icons/fa6";
+import { IoMdCheckboxOutline } from "react-icons/io";
 
-const Task = ({Task , onDelete}) => {
+
+const Task = ({Task , onDelete , checkTask}) => {
 
           
     return (
-        <div className='task'>
-            <h1>{Task.title}</h1>
-            <button onClick={() =>onDelete(Task.id)}>
-                click
-            </button>
+        <div style={{backgroundColor : Task.value ? `hsl(213, 47%, 75%)` : `hsl(213, 12.80%, 47.60%)` }} className='task'>
+            <h1>
+                {Task.title}
+            </h1>
+
+
+            <div>
+                <IoMdCheckboxOutline onClick={() => checkTask(Task.id)}/>
+                <FaTrash onClick={() =>onDelete(Task.id)} />
+            </div>
+
         </div>
 
     )
